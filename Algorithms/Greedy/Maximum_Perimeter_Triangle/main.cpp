d2194d88976698bb41d3a6b319b0f099f74e23eb
Ugurcan <ugur.snmz95@gmail.com>
2020-08-10 21:06:55 +0300

    modified file

--- Algorithms/Greedy/Maximum_Perimeter_Triangle/main.cpp
+++ Algorithms/Greedy/Maximum_Perimeter_Triangle/main.cpp
@@ -4,15 +4,19 @@
 #include <vector>
 #include <algorithm>
 
+bool checkTriangle(int num1, int num2, int num3) {
+    return num1+num2 > num3;
+}
+
 void maximumPerimeterTriangle(std::vector<int> sticks) {
     std::sort(sticks.begin(), sticks.end());
-    int idx {sticks.size()-3};
-    while (idx >= 0 && sticks[idx]+sticks[idx+1] <= sticks[idx+2])
-        idx--;
-    if (idx >= 0)
-        std::cout << sticks[idx] << " " << sticks[idx+1] << " " << sticks[idx+2];
-    else
-        std::cout << -1;
+    for (int i{sticks.size()-1}; i >= 2; i--) {
+        if (checkTriangle(sticks[i-2], sticks[i-1], sticks[i])) {
+            std::cout << sticks[i-2] << " " << sticks[i-1] << " " << sticks[i];
+            return;
+        } 
+    }
+    std::cout << -1;
 }
 
 int main() {
--- Algorithms/Greedy/Maximum_Perimeter_Triangle/main.py
+++ Algorithms/Greedy/Maximum_Perimeter_Triangle/main.py
@@ -1,12 +1,14 @@
 ### Maximum Perimeter Triangle - Solution
 
+def checkTriangle(num1, num2, num3):
+    return num1+num2 > num3
+
 def maximumPerimeterTriangle(sticks):
     sticks.sort()
-    idx = len(sticks)-3
-    while (idx >= 0) and (sticks[idx]+sticks[idx+1] <= sticks[idx+2]):
-        idx -= 1
-    if idx >= 0:
-        print(sticks[idx], sticks[idx+1], sticks[idx+2])
+    for i in range(len(sticks)-1, 1, -1):
+        if checkTriangle(sticks[i-2], sticks[i-1], sticks[i]):
+            print(sticks[i-2], sticks[i-1], sticks[i])
+            break
     else:
         print(-1)
 
